bf125758620085e7ba8b5e8a296fc20731aea475
ASCM5 <utm22030590@utma.edu.mx>
2025-03-27 14:25:38 -0600

    feat: urls creadas en myapp, añadi myapp a settings en mypage, cree las migraciones para bases de datos

--- mypage/urls.py
+++ mypage/urls.py
@@ -15,11 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from myapp import views
+from django.urls import path, include #Include sirve para importar urls de apps
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.hello), #Ruta principal
-    path('about/', views.about)
-
+    path('', include('myapp.urls'))
 ]
