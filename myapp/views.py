fd1cba82b095d93893f77798e4db03d20d4fd9c0
ASCM5 <utm22030590@utma.edu.mx>
2025-03-28 17:57:46 -0600

    feat: uso de render para cargar paginas

--- myapp/views.py
+++ myapp/views.py
@@ -1,21 +1,27 @@
 from django.http import HttpResponse, JsonResponse
 from .models import Project, Task
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, render
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Index Page')
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
 
 def hello(request, username):
     return HttpResponse('<h1>Hello %s</h1>' %username)
 
-def about(request):
-    return HttpResponse('<h2>About</h2>')
+# def projects(request):
+#     projects = list(Project.objects.values())#Query-Set
+#     return JsonResponse(projects, safe=False) #Mandamos en forma de json
 
 def projects(request):
-    projects = list(Project.objects.values())#Query-Set
-    return JsonResponse(projects, safe=False) #Mandamos en forma de json
+    return render(request, 'projects.html')
 
-def task(request, id):
-    task = get_object_or_404(Task, id=id) #Obtener objeto, si no, error 404
-    return HttpResponse('task: %s' % task.title)
+# def task(request, id):
+#     task = get_object_or_404(Task, id=id) #Obtener objeto, si no, error 404
+#     return HttpResponse('task: %s' % task.title)
+
+def task(request):
+    return render(request, 'task.html')
